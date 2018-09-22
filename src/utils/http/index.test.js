8191846556e20179cf97b88109e1cf663d370bfb
Tony <tonyblast@gmail.com>
2018-09-22 14:01:05 -0300

    [tests] test: Added test for HTTP util

---
+++ src/utils/http/index.test.js
@@ -0,0 +1,20 @@
+/* global describe it expect */
+
+import HTTP from './';
+
+import Config from 'Config/';
+
+describe('HTTP class', () => {
+    it('should create an instance with defaults', () => {
+        const http = new HTTP();
+
+        expect(http).toBeInstanceOf(HTTP);
+        expect(http.instance).toBeInstanceOf(Function);
+        expect(http.instance.defaults).toHaveProperty(
+            'headers',
+            expect.objectContaining({
+                'User-Agent': expect.stringContaining(Config.get('userAgent'))
+            })
+        ); 
+    });
+});
