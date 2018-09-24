6f6b8793b41495ac419f7675715449b0299d86a4
Tony <tonyblast@gmail.com>
2018-09-24 07:27:39 -0300

    [eslint-refactor] test: Fixed test lint 'no-magic-number'

--- src/utils/helpers/units.test.js
+++ src/utils/helpers/units.test.js
@@ -5,7 +5,7 @@ import { DAY, toMilliseconds } from './units';
 describe('Helpers: Units', () => {
     it('should return a time in milliseconds', () => {
         const inMs = toMilliseconds(DAY);
-        const expected = DAY * 1000;
+        const expected = DAY * 1000; // eslint-disable-line no-magic-numbers
 
         expect(inMs).toEqual(expected);
     });
