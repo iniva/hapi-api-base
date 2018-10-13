aeab17d91d612e60814d8b1f5c2d801983cc9a19
Tony <tonyblast@gmail.com>
2018-10-13 19:46:17 -0300

    [incoming-webhooks] feat(slack github webhook): Added 'issues' support

--- src/utils/slack/webhook/github/events.js
+++ src/utils/slack/webhook/github/events.js
@@ -10,10 +10,12 @@ const getAuthorDetails = author => {
     };
 };
 
+const getRepoName = repository => repository.full_name.split('/').pop();
+
 export const release = payload => {
     const { release: { html_url, tag_name, author, published_at }, repository } = payload;
     const authorDetails = getAuthorDetails(author);
-    const repoName = repository.full_name.split('/').pop();
+    const repoName = getRepoName(repository);
 
     return {
         username,
@@ -26,6 +28,41 @@ export const release = payload => {
                 title: `Release ${tag_name}`,
                 title_link: html_url,
                 ts: Date.parse(published_at) / 1000 // eslint-disable-line no-magic-numbers
+            }
+        ]
+    };
+};
+
+export const issues = payload => {
+    const { action, issue, repository } = payload;
+    const { html_url, number, title, user, labels, state, updated_at } = issue;
+    const authorDetails = getAuthorDetails(user);
+    const repoName = getRepoName(repository);
+
+    const labelField = {
+        title: 'labels',
+        value: labels.map(label => label.name).join(', '),
+        'short': true
+    };
+    const stateField = {
+        title: 'state',
+        value: state,
+        'short': true
+    };
+    const fields = [labelField, stateField];
+
+    return {
+        username,
+        attachments: [
+            {
+                fallback: `Issue #${number}: ${title}`,
+                pretext: `Issue ${action} in *${repoName}*`,
+                color: '#36a64f',
+                ...authorDetails,
+                title: `Issue #${number}: ${title}`,
+                title_link: html_url,
+                fields,
+                ts: Date.parse(updated_at) / 1000 // eslint-disable-line no-magic-numbers
             }
         ]
     };
