2ef223c15daebbc55c22b4e625082de713678ccb
Tony <tonyblast@gmail.com>
2018-10-16 06:29:28 -0300

    [discovery-service] refactor: Added DISCOVERY_SERVICE_MASK env variable

--- src/plugins/services/discovery/index.js
+++ src/plugins/services/discovery/index.js
@@ -1,14 +1,14 @@
 const name = 'discovery-service';
+const DISCOVERY_SERVICE_MASK = process.env.DISCOVERY_SERVICE_MASK || 'service::';
 
 export default {
     name,
     register: async server => {
         const getAll = () => {
-            const serviceMask = 'service::';
             const services = Object.keys(server)
-                .filter(key => key.includes(serviceMask))
+                .filter(key => key.includes(DISCOVERY_SERVICE_MASK))
                 .reduce((obj, key) => {
-                    const service = key.replace(serviceMask, '');
+                    const service = key.replace(DISCOVERY_SERVICE_MASK, '');
 
                     obj[service] = server[key];
                     return obj;
@@ -27,6 +27,6 @@ export default {
             return services[service];
         };
 
-        server.decorate('server', 'service::discovery', { get, getAll });
+        server.decorate('server', `${DISCOVERY_SERVICE_MASK}discovery`, { get, getAll });
     }
 };
