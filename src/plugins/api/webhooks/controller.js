b6250d99fe9b9e1beb222d16d9d8b296b9c8dc6f
Tony <tonyblast@gmail.com>
2018-11-09 06:33:01 -0300

    [slack-webhooks-refactor] refactor: Use new Discovery Service name in Webhooks controller

--- src/plugins/api/webhooks/controller.js
+++ src/plugins/api/webhooks/controller.js
@@ -2,7 +2,7 @@ import Boom from 'boom';
 
 export default class WebhooksController {
     static async get(request) {
-        const slackService = request.server['service::discovery'].get('slack');
+        const slackService = request.server.discoveryService.get('slack');
 
         return {
             data: slackService.webhookList()
@@ -12,7 +12,7 @@ export default class WebhooksController {
     static async trigger(request) {
         try {
             const { headers, params, payload } = request;
-            const slackService = request.server['service::discovery'].get('slack');
+            const slackService = request.server.discoveryService.get('slack');
 
             return await slackService
                 .eventFrom(params.type, headers, payload)
