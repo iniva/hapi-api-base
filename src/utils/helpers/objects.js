6f2cd7fd76065ca5c759200e38844ed5598bb0d8
Tony <tonyblast@gmail.com>
2019-02-17 07:44:44 -0300

    [adopt-eslint-airbnb] refactor: Refactored object "clean" helper

--- src/utils/helpers/objects.js
+++ src/utils/helpers/objects.js
@@ -11,19 +11,39 @@ export const hasOwnProperty = (obj, key) => Object.prototype.hasOwnProperty.call
 
 /**
  * @function clean
- * @description Cleans an object from invalid values
+ * @description Cleans an object from forbidden values
+ * By default, forbidden values are [undefined, null, false]
  *
  * @param {Object} rawObject
+ * @param {Object} options
  *
  * @returns {Object}
  */
-export const clean = (rawObject, customGuard) => {
-  const defaultGuard = (object, key) => (
-    object[key] !== null && object[key] !== false && object[key] !== undefined
-  );
-  const guard = customGuard || defaultGuard;
+export const clean = (rawObject, options = {}) => {
+  const defaults = {
+    _undefined: false,
+    _null: false,
+    _false: false,
+    ...options,
+  };
+  const values = {
+    _undefined: undefined,
+    _null: null,
+    _false: false,
+  };
+
+  const forbidden = Object.keys(defaults).reduce((acc, option) => {
+    if (!defaults[option]) {
+      acc.push(values[option]);
+    }
+
+    return acc;
+  }, []);
+
+  const isAllowed = (object, key) => !forbidden.includes(object[key]);
+
   const newObj = Object.keys(rawObject).reduce((obj, key) => {
-    if (guard(rawObject, key)) {
+    if (isAllowed(rawObject, key)) {
       obj[key] = rawObject[key];
     }
 
