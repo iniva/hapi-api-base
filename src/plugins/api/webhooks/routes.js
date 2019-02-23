c2626dc2c6b8f24646b85ed717dacc50bf3569b9
Tony <tonyblast@gmail.com>
2019-02-23 08:09:22 -0300

    [master] refactor: Renamed 'prefix' variable in routes to 'API_PATH'

--- src/plugins/api/webhooks/routes.js
+++ src/plugins/api/webhooks/routes.js
@@ -1,11 +1,11 @@
 import WebhooksController from './controller';
 
-const prefix = 'webhooks';
+const API_PATH = '/webhooks';
 
 export default [
   {
     method: 'GET',
-    path: `/${prefix}`,
+    path: API_PATH,
     options: {
       description: 'List available webhooks',
       handler: WebhooksController.get,
@@ -14,7 +14,7 @@ export default [
 
   {
     method: 'POST',
-    path: `/${prefix}/{type}`,
+    path: `${API_PATH}/{type}`,
     options: {
       description: 'Webhooks Trigger',
       handler: WebhooksController.trigger,
