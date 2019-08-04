468653e6bdcd9d8d6d656370d43347d9b00c04a7
Tony DBR <tonyblast@gmail.com>
2019-08-04 16:55:54 -0300

    [master] feat: Enabled https for local dev. Added dev certificates"

--- src/config/development.js
+++ src/config/development.js
@@ -1,7 +1,13 @@
+const fs = require('fs');
+
 const development = {
   server: {
     debug: {
       request: ['*'],
+    },
+    tls: {
+      key: fs.readFileSync(`${__dirname}/../../localhost-key.pem`),
+      cert: fs.readFileSync(`${__dirname}/../../localhost.pem`),
     },
   },
 };
